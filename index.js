ac3bf3656bbf721fad2f726cae091794f3094536
Justin Li <justinlit23@gmail.com>
2022-04-05 08:12:13 -0700

    commit tree 61d99234d25f794151908d58d13ab2ba3de31b21

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (streetNumber) {
+    return Math.abs(streetNumber - 42);
+}
+
+function distanceFromHqInFeet (streetNumber) {
+    const distance = distanceFromHqInBlocks(streetNumber) * 264;
+    return distance;
+}
+
+function distanceTravelledInFeet (startBlock, endBlock) {
+    return (Math.abs(endBlock - startBlock) * 264);
+    
+}
+
+
+function calculatesFarePrice (startBlock, endBlock) {
+    let distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+    if (distanceInFeet < 400) {
+        return 0;
+    } else if (distanceInFeet < 2000 ) {
+        return (distanceInFeet - 400 ) / 50;
+    } else if (distanceInFeet < 2500 ) {
+        return 25;
+    } else if (distanceInFeet > 2500 ) {
+        return 'cannot travel that far';
+    }
+}
